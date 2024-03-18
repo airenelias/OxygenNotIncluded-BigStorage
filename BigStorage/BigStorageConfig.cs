edbc518f9d5483bd826766d109dd1824f0356b2e
Airen <airenilia@gmail.com>
2024-03-19 03:21:07 +0700

    Big Gas Reservoir tweak, mod capacity settings limits tweak

--- BigStorage/BigStorageConfig.cs
+++ BigStorage/BigStorageConfig.cs
@@ -12,37 +12,37 @@ namespace BigStorage
         [JsonProperty]
         [Option("STRINGS.UI.CAPACITY.BIGSTORAGELOCKER.TITLE",
             "STRINGS.UI.CAPACITY.BIGSTORAGELOCKER.TOOLTIP", Format = "F0")]
-        [Limit(1000, 1000000)]
+        [Limit(2000, 200000)]
         public int BigStorageLockerCapacity { get; set; } = 80000;
 
         [JsonProperty]
         [Option("STRINGS.UI.CAPACITY.BIGBEAUTIFULSTORAGELOCKER.TITLE",
             "STRINGS.UI.CAPACITY.BIGBEAUTIFULSTORAGELOCKER.TOOLTIP", Format = "F0")]
-        [Limit(1000, 1000000)]
+        [Limit(2000, 200000)]
         public int BigBeautifulStorageLockerCapacity { get; set; } = 80000;
 
         [JsonProperty]
         [Option("STRINGS.UI.CAPACITY.BIGSMARTSTORAGELOCKER.TITLE",
             "STRINGS.UI.CAPACITY.BIGSMARTSTORAGELOCKER.TOOLTIP", Format = "F0")]
-        [Limit(1000, 1000000)]
+        [Limit(2000, 200000)]
         public int BigSmartStorageLockerCapacity { get; set; } = 80000;
 
         [JsonProperty]
         [Option("STRINGS.UI.CAPACITY.BIGLIQUIDSTORAGE.TITLE",
             "STRINGS.UI.CAPACITY.BIGLIQUIDSTORAGE.TOOLTIP", Format = "F0")]
-        [Limit(1000, 100000)]
+        [Limit(500, 50000)]
         public int BigLiquidStorageCapacity { get; set; } = 20000;
 
         [JsonProperty]
         [Option("STRINGS.UI.CAPACITY.BIGGASSTORAGE.TITLE",
             "STRINGS.UI.CAPACITY.BIGGASSTORAGE.TOOLTIP", Format = "F0")]
         [Limit(100, 10000)]
-        public int BigGasStorageCapacity { get; set; } = 600;
+        public int BigGasStorageCapacity { get; set; } = 4000;
 
         [JsonProperty]
         [Option("STRINGS.UI.CAPACITY.BIGSTORAGETILE.TITLE",
             "STRINGS.UI.CAPACITY.BIGSTORAGETILE.TOOLTIP", Format = "F0")]
-        [Limit(100, 1000000)]
+        [Limit(100, 10000)]
         public int BigStorageTileCapacity { get; set; } = 4000;
     }
 }
